e3bc4ec74a12bbba6d344edee8beef8c54289257
leedongbin <lcw245@everon.co.kr>
2023-09-06 14:31:05 +0900

    1234444

--- routes/goladream.js
+++ routes/goladream.js
@@ -2,7 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.send('hello world')
+    const choosePlace = () => {
+        const placeList = ["세무서","유안타","회식"]
+        const randomIndex = Math.floor(Math.random() * placeList.length);
+        return placeList[randomIndex]
+    }
+    res.send(choosePlace)
 });
-//2
+
 module.exports = router;
