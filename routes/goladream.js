2ec0232f305cba6676176b6eafcf7c5489beec80
tinder <93254567+Suchlikesamsung@users.noreply.github.com>
2023-09-06 15:29:09 +0900

    Update!! goladream.js

--- routes/goladream.js
+++ routes/goladream.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/goladream', function(req, res, next) {
+// POST 요청에 대한 라우터 정의
+router.post('/goladream', function(req, res, next) {
     const choosePlace = () => {
         const placeList = ["세무서","유안타","회식"]
         const randomIndex = Math.floor(Math.random() * placeList.length);
@@ -10,7 +11,6 @@ router.get('/goladream', function(req, res, next) {
     const response = {
         "text" : choosePlace(),
         "responseType" : "inChannel"
-
     }
     res.json(response);
 });
