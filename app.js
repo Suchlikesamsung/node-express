f1607ca18f979bc0e698c305f152b294e09ddb15
leedongbin <lcw245@everon.co.kr>
2023-09-06 15:56:52 +0900

    dd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const nunjucks = require('nunjucks');
+// const nunjucks = require('nunjucks');
 
 var indexRouter = require('./routes/index');
 var golaRouter = require('./routes/goladream');
@@ -12,12 +12,12 @@ var golaRouter = require('./routes/goladream');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'njk');
-nunjucks.configure('views', { 
-  express: app,
-  watch: true,
-});
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'njk');
+// nunjucks.configure('views', { 
+//   express: app,
+//   watch: true,
+// });
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -27,7 +27,20 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 // app.use('/users', usersRouter);
-app.use('/goladream', golaRouter);
+app.post('/goladream', function(req, res, next) {
+  const data = req.body;
+  console.log(data)
+  const choosePlace = () => {
+      const placeList = ["세무서","유안타","회식"]
+      const randomIndex = Math.floor(Math.random() * placeList.length);
+      return placeList[randomIndex]
+  }
+  const response = {
+      "text" : choosePlace(),
+      "responseType" : "inChannel"
+  }
+  res.status(200).json(response);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,13 +54,21 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // Set the response content type to JSON
+  res.setHeader('Content-Type', 'application/json');
 
-  // render the error page
+  // Create a JSON response with error information
+  const errorResponse = {
+    message: err.message,
+    error: req.app.get('env') === 'development' ? err : {}
+  };
+
+  // Set the status code to the error status or 500 (Internal Server Error) if not available
   res.status(err.status || 500);
-  res.render('error');
+
+  // Send the JSON response
+  res.send(JSON.stringify(errorResponse));
 });
+
 
 module.exports = app;
--- routes/goladream.js
+++ routes/goladream.js
@@ -3,6 +3,8 @@ var router = express.Router();
 
 // POST 요청에 대한 라우터 정의
 router.post('/goladream', function(req, res, next) {
+    const data = req.body;
+    console.log(data)
     const choosePlace = () => {
         const placeList = ["세무서","유안타","회식"]
         const randomIndex = Math.floor(Math.random() * placeList.length);
@@ -12,7 +14,7 @@ router.post('/goladream', function(req, res, next) {
         "text" : choosePlace(),
         "responseType" : "inChannel"
     }
-    res.json(response);
+    res.status(200).json(response);
 });
 
 module.exports = router;
